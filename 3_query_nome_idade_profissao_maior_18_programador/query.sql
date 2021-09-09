3dc32c49aa04272e80bfab042f822c03e71d5166
streeg <140141961@aluno.unb.br>
2021-09-09 12:05:59 -0300

    exercize 2 table and changing names to fit a pattern

--- 3_query_nome_idade_profissao_maior_18_programador/query.sql
+++ 3_query_nome_idade_profissao_maior_18_programador/query.sql
@@ -1,4 +1,9 @@
 SELECT users.name, users.age, professions.name 
 FROM users INNER JOIN professions 
 ON users.profession_id = professions.id 
-WHERE users.age > 18 AND professions.name = 'Programador';
+WHERE users.age > 18 AND (professions.name = 'Programador' OR professions.name = 'Desenvolvedor');
+
+/*Alternativamente numa mesma tabela*/
+
+SELECT users.name, users.age, users.professions
+WHERE (users.age > 18 AND users.professions = 'Programador') OR (users.age > 18 AND users.professions = 'Desenvolvedor');
